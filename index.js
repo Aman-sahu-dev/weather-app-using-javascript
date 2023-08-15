35bfb6eee0eaf21a019954d554541f8e5c94063c
Aman <amansahu.dev@gmail.com>
2023-08-15 19:56:21 +0530

    second commit

--- index.js
+++ index.js
@@ -25,22 +25,26 @@ function displayResults(weather) {
     let now = new Date();
     let date = document.querySelector('.location .date');
     let temp = document.querySelector('.temp');
+    let temp2 = document.querySelector('.temp2');
     let weather_el = document.querySelector('.weather');
     let hilow = document.querySelector('.hi-low');
     let humidity = document.querySelector('#hval');
     let visibility = document.querySelector('#vval');
     let windspeed = document.querySelector('#speed');
     let icon = document.querySelector('#cloudicon');
+    let pressure = document.querySelector('#pressure');
 
     city.innerText = `${weather.name}, ${weather.sys.country}`;
     date.innerText = dateBuilder(now);
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-    weather_el.innerText = weather.weather[0].main;
+    temp2.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+    weather_el.innerText = weather.weather[0].description;
     icon.innerHTML = `<img src="https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png">`;
     hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
-    humidity.innerText = `${weather.main.humidity}`;
-    visibility.innerText = `${weather.visibility}`;
-    windspeed.innerText = `${weather.wind.speed}`;
+    humidity.innerText = `${weather.main.humidity}%`;
+    visibility.innerText = `${weather.visibility/1000} km`;
+    windspeed.innerText = `${weather.wind.speed}m/s`;
+    pressure.innerText = `${weather.main.pressure} hPa`;
 
 }
 
